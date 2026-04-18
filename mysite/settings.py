4fbb949c6e9dd3a769c4fb2472cf1caaa8f1cc37
ЙЙЙЙЙЙ <124801653+QWERTYMARIA1111@users.noreply.github.com>
2026-04-18 18:35:39 +0400

    modified:   mysite/settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-#$m=l9m9#v@$e@eugqe0e6d$bj&!gdzzsbkg)e!yaorj%pz3mz'
